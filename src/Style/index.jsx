f6933b458c31614f0af91a7f78a0b5a17aad37d1
Vincenzofdg <vincenzofdg.dev@gmail.com>
2024-02-21 21:17:55 -0300

    feat: tab menu

--- src/Style/index.jsx
+++ src/Style/index.jsx
@@ -1,12 +1,7 @@
 import Cover from "./Screens/Cover";
 import Login from "./Screens/Login";
-// import test from './Componente/test';
 
 export const screens = {
     Cover,
     Login
 };
-
-// export const components = {
-//     test
-// };
