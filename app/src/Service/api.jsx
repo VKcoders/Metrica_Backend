97e2232b9007279484161bc67afbf8c2a2a29d8b
Vincenzofdg <vincenzofdg.dev@gmail.com>
2024-03-12 17:33:10 -0300

    feat: done

--- app/src/Service/api.jsx
+++ app/src/Service/api.jsx
@@ -6,8 +6,8 @@ const {url, prefix} = {
 }
 
 const api = axios.create({
-  // baseURL: url + prefix,
-  baseURL: "http://192.168.0.51:3002/pesquisa"
+  baseURL: url + prefix,
+  // baseURL: "http://192.168.0.51:3002/pesquisa"
   // baseURL: "http://localhost:3002/pesquisa"
   // baseURL: "http://10.0.2.2:3002/pesquisa"
 });
