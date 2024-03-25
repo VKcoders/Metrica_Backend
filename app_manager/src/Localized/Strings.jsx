31cf199aff20713cc7c40cd7a3d00f8efb692579
Vincenzofdg <vincenzofdg.dev@gmail.com>
2024-03-25 19:28:10 -0300

    feat: app_manager

---
+++ app_manager/src/Localized/Strings.jsx
@@ -0,0 +1,28 @@
+const strings = {
+     Login: {
+          title: "Métrica Gestor",
+          username: ["Usuário", "digite seu usuário"],
+          password: ["Senha", "digite sua senha"],
+          submitBtn: "Entrar"
+     },
+     Remainder: {
+          new: "Criar um lembrete",
+          add: "Adicionar mais um lembrete",
+          remove: "Remover Lembrete",
+          edit: "Editar lembrete"
+     },
+     Announcement: {
+          noAnnouncement: "Sem avisos até o momento..."
+     },
+     Report: {
+          new: "Criar uma ocorrência",
+          add: "Adicionar mais um ocorrência",
+          remove: "Remover ocorrência",
+          edit: "Editar ocorrência"
+     },
+     Modal: {
+          Login: ["Usuário ou Senha inválido.", "Confirmar"],
+     },
+};
+
+module.exports = strings;
---
+++ app_manager/src/Style/index.jsx
@@ -0,0 +1,14 @@
+import Login from "./Screens/Login";
+import Home from "./Screens/Home";
+import Background from "./Components/Background";
+import LoginModal from "./Modal/Login";
+
+export const screens = {
+    // Screens
+    Login,
+    Home,
+    // Components
+    Background,
+    // Modal
+    LoginModal,
+};
